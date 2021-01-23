31bd86e9aead63725523316b6874111ac5d45492
Kartik Malik <kartikmalik91@gmail.com>
2021-01-23 22:34:08 +0530

    Product sort feature

--- server/api/product/product.controller.js
+++ server/api/product/product.controller.js
@@ -1,11 +1,14 @@
-const { retrieveProducts } = require("./product.service");
+const { retrieveProducts, searchProducts } = require("./product.service");
 
 const getProductsController = async (req, res, next) => {
   console.log(req.query);
   let limit = parseInt(req.query.limit) || 100;
   let skip = parseInt(req.query.skip) || 0;
+  let name = req.query.name && req.query.name.trim();
   try {
-    let products = await retrieveProducts(limit, skip);
+    let products;
+    if (name) products = await searchProducts(name, limit, skip);
+    else products = await retrieveProducts(limit, skip);
     res.status(200).send({ products });
   } catch (e) {
     // console.error(e.message);
--- server/api/product/product.service.js
+++ server/api/product/product.service.js
@@ -1,4 +1,5 @@
 const ProductModel = require("./product.model");
+const escapeStringRegexp = require("escape-string-regexp");
 
 const retrieveProducts = async (limit = 100, skip = 0) => {
   try {
@@ -9,6 +10,41 @@ const retrieveProducts = async (limit = 100, skip = 0) => {
   }
 };
 
+const searchProducts = async (name, limit = 100, skip = 0) => {
+  try {
+    const regex = escapeStringRegexp(name);
+    console.log("reges", regex);
+
+    let products = await ProductModel.aggregate([
+      { $match: { name: { $regex: regex, $options: "i" } } },
+      {
+        $project: {
+          type: 1,
+          _id: 1,
+          authors: 1,
+          average_rating: 1,
+          isbn: 1,
+          language_code: 1,
+          ratings_count: 1,
+          price: 1,
+          name: { $substr: ["$name", 0, 100] },
+        },
+      },
+    ])
+      .skip(skip)
+      .limit(limit);
+
+    // let products = await ProductModel.find({
+    //   name: { $regex: regex, $options: "i" },
+    // }).skip(skip)
+    //   .limit(limit);
+    return products;
+  } catch (e) {
+    throw new Error(e.message);
+  }
+};
+
 module.exports = {
   retrieveProducts,
+  searchProducts,
 };
